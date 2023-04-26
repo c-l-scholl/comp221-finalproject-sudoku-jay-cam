067ad6ce58f5e6c5de86331f1a0a5b4902b7381b
qayjjj <trngquanh@gmail.com>
2023-04-26 11:02:48 -0500

    update checkBox

--- src/sudoku.js
+++ src/sudoku.js
@@ -2,15 +2,22 @@
 // if testNum doesn't exist in the row, index will be -1
 
 const checkRow = (array, currCell, testNum) => {
-	return array[currCell.rowIndex].indexOf(testNum) === -1;
-};
+  return array[currCell.rowIndex].indexOf(testNum) === -1
+}
 
 // currCell should be empty
 // checking testNum doesn't exist in the column
 const checkColumn = (array, currCell, testNum) => {
-	return !array.some(row => row[currCell.collIndex] === testNum);
-};
+  return !array.some((row) => row[currCell.collIndex] === testNum)
+}
 
 const checkBox = (array, currCell, testNum) => {
-
-};
+  const boxRow = currCell.rowIndex % 3
+  const boxCol = currCell.collIndex % 3
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (array[boxRow + i][boxCol + j] === testNum) return false
+    }
+  }
+  return true
+}
